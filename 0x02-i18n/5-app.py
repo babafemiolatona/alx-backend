073b7f503ff4098ffec70675a3a00e6e116ccab1
babafemiolatona <femiolatona@gmail.com>
2024-02-06 23:00:39 +0100

    Use user locale

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""A Basic Flask app.
-"""
-
+"""A Basic Flask app."""
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+from flask import (
+    Flask,
+    render_template,
+    request,
+    g
+)
+from flask_babel import Babel
+from typing import (
+    Dict,
+    Union
+)
+
+
+class Config(object):
+    """
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    """
+    Returns a user dictionary or None if ID value can't be found
+    or if 'login_as' URL parameter was not found
+    """
+    user_id = request.args.get('login_as')
+    return users.get(int(user_id)) if user_id else None
+
+
+@app.before_request
+def before_request():
+    """
+    Add user to flask.g if user is found
+    """
+    user = get_user()
+    g.user = user
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Select and return best language match based on supported languages
+    """
+    supported_languages = app.config['LANGUAGES']
+    preferred_locales = [request.args.get('locale'), g.user.get('locale'), request.headers.get('locale')]
+    for locale in preferred_locales:
+        if locale in supported_languages:
+            return locale
+    return request.accept_languages.best_match(supported_languages)
+
+
+@app.route('/', strict_slashes=False)
+def home_index() -> str:
+    """
+    Handles / route
+    """
+    return render_template('6-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
