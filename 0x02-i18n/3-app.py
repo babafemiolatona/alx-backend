c8a8f759b9f2f5a731534b200453d5368c3cb56d
babafemiolatona <femiolatona@gmail.com>
2024-02-06 22:41:06 +0100

    Force locale with URL parameter

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -25,7 +25,7 @@ def get_locale() -> str:
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """Index page"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -19,13 +19,16 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale() -> str:
     """Get locale"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """Index page"""
-    return render_template('2-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
