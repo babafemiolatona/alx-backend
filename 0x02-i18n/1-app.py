d7a13ed12b409e90909ff100ab500f99b4095e5f
babafemiolatona <femiolatona@gmail.com>
2024-02-06 21:47:54 +0100

    Basic babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
-from flask import Flask
+from flask import Flask, render_template
 from flask_babel import Babel
 
 
@@ -14,3 +14,13 @@ class Config:
 app = Flask(__name__)
 app.config.from_object('Config')
 babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """Index page"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
