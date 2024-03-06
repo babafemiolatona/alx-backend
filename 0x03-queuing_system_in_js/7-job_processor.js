de4e346bc3e2c01693f4c0ffa7d48e24dbd1c079
babafemiolatona <femiolatona@gmail.com>
2024-03-06 15:55:00 +0100

    Writing the job creation function

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -4,26 +4,26 @@ const queue = kue.createQueue();
 
 const blacklistedNumbers = [4153518780, 4153518781];
 
-function sendNotification(phoneNumber, message, job, done) {
-    if (blacklistedNumbers.includes(phoneNumber)) {
-        const error = new Error(`Phone number ${phoneNumber} is blacklisted`);
-        return done(error);
-    }
+function sendNotification (phoneNumber, message, job, done) {
+  if (blacklistedNumbers.includes(phoneNumber)) {
+    const error = new Error(`Phone number ${phoneNumber} is blacklisted`);
+    return done(error);
+  }
 
-    job.progress(50);
+  job.progress(50);
 
-    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
-    done();
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+  done();
 }
 
 queue.process('push_notification_code_2', 2, (job, done) => {
-    job.progress(0);
+  job.progress(0);
 
-    sendNotification(job.data.phoneNumber, job.data.message, job, done);
+  sendNotification(job.data.phoneNumber, job.data.message, job, done);
 });
 
 queue.on('error', (err) => {
-    console.error('Queue error:', err);
+  console.error('Queue error:', err);
 });
 
 console.log('Jobs processing started...');
